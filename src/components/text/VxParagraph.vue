6ea3354c2fd2d28241bd7792ebadcb9c21c1b0fd
David Domkář <dave.domkar@gmail.com>
2020-05-05 01:57:03 +0200

    feat(components): vx-paragraph

---
+++ src/components/text/VxParagraph.vue
@@ -0,0 +1,11 @@
+<template>
+  <vx-tag tag="p">
+    <slot />
+  </vx-tag>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({});
+</script>
